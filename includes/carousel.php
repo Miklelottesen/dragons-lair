9a0e7dcafe0bbd808feac88f15e826a2b0c4e14a
viliusbivainis <viliusbiv@gmail.com>
2014-12-13 20:20:40 +0100

    Adding bootstrap carousel/image slider picture 1/placeholders

---
+++ includes/carousel.php
@@ -0,0 +1,38 @@
+<section id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <section class="carousel-inner">
+        <section class="item active">
+            <img src="./img/slider_1.png" alt="First slider picture">
+            <section class="carousel-caption">
+                <h3>Caption Text</h3>
+            </section>
+        </section>
+        <section class="item">
+            <img src="http://placehold.it/1914x468" alt="...">
+            <section class="carousel-caption">
+                <h3>Caption Text</h3>
+            </section>
+        </section>
+        <section class="item">
+            <img src="http://placehold.it/1914x468" alt="...">
+            <section class="carousel-caption">
+                <h3>Caption Text</h3>
+            </section>
+        </section>
+    </section>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+</section> <!-- Carousel -->
--- index.php
+++ index.php
@@ -20,11 +20,16 @@
 		// Construct URL of the content file to get
 	include 'includes/header.php';
 	// Get the header
-?> 
+
+	if ( empty( $_GET )){
+		include './includes/carousel.php';
+	// Display Bootstrap Carousel on any other page than Home or Post view (which has no page_id)
+	}
+?>
 	<main class="container-fluid">
 		<section id="content-area" class="container">
 
-		<?php 
+		<?php
 			if ( !empty( $_GET )){
 				include 'includes/sidebar.php'; 
 				// Display sidebar on any other page than Home or Post view (which has no page_id)
