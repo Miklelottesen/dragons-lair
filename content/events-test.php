7c92b0d727a1bd6398c472d98a702d9ec3b8025d
Miklelottesen <miklelottesen@gmail.com>
2014-12-15 00:14:47 +0100

    events test added

---
+++ content/events-test.php
@@ -0,0 +1,5 @@
+<article>
+	<h2>Who can pickpocket the most guys in 30 mins?</h2>
+	<small>Somestreet | Something | Yeahm</small>
+	<p>Find out if you have what it takes to join a gang og cockney pickpockets. You'll be let loose in the city, where you will pickpocket. The participants who brings the most loot back to base wins a free t-shirt. Penalty if you get caught by the police.</p>
+</article>
