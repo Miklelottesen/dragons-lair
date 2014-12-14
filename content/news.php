decd1a725f23f8f1ce79cbcfff3f549cf471467c
viliusbivainis <viliusbiv@gmail.com>
2014-12-14 21:02:00 +0100

    news styling

--- content/news.php
+++ content/news.php
@@ -1,63 +1,52 @@
 <hr id="red-nav-line" />
-<section class="row">
-
-    <section class="col-md-8">
-        <div class="span">
-            <img src="./img/news/new-masks-arrival.jpg" alt="...">
-            <div>
-                <h3>Thumbnail label</h3>
-                <p>...</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
+<div class="container">
+    <div class="row clearfix">
+        <div class="col-md-8 column">
+            <img alt="140x140" src="http://lorempixel.com/140/140/" />
+            <h3>
+                h3. Lorem ipsum dolor sit amet.
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+            </p>
         </div>
-    </section>
-
-    <section class="col-md-4">
-        <div class="span">
-            <img src="./img/news/new-masks-arrival.jpg" alt="...">
-            <div>
-                <h3>Thumbnail label</h3>
-                <p>...</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
+        <div class="col-md-4 column">
+            <img alt="140x140" src="http://lorempixel.com/140/140/" />
+            <h3>
+                h3. Lorem ipsum dolor sit amet.
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+            </p>
         </div>
-    </section>
-    
-</section>
-<section class="row">
-
-    <section class="col-md-4">Lol
-    </section>
-
-    <section class="col-md-4">Lol
-    </section>
-
-    <section class="col-md-4">Lol
-    </section>
-</section>
-
-    <section class="row">
-
-        <section class="col-md-4">Lol
-        </section>
-
-        <section class="col-md-4">Lol
-        </section>
-
-        <section class="col-md-4">Lol
-        </section>
-    </section>
-
-        <section class="row">
-
-            <section class="col-md-4">Lol
-            </section>
-
-            <section class="col-md-4">Lol
-            </section>
-
-            <section class="col-md-4">Lol
-            </section>
-        </section>
-
-</section>
+    </div>
+    <div class="row clearfix">
+        <div class="col-md-4 column">
+            <img alt="140x140" src="http://lorempixel.com/140/140/" />
+            <h3>
+                h3. Lorem ipsum dolor sit amet.
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+            </p>
+        </div>
+        <div class="col-md-4 column">
+            <img alt="140x140" src="http://lorempixel.com/140/140/" />
+            <h3>
+                h3. Lorem ipsum dolor sit amet.
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+            </p>
+        </div>
+        <div class="col-md-4 column">
+            <img alt="140x140" src="http://lorempixel.com/140/140/" />
+            <h3>
+                h3. Lorem ipsum dolor sit amet.
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+            </p>
+        </div>
+    </div>
+</div>
