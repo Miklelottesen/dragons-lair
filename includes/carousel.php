f4ce20b5c47856515c1a1614777893e9683578a8
viliusbivainis <viliusbiv@gmail.com>
2014-12-15 02:49:05 +0100

    added call-to-action for home carousel

--- includes/carousel.php
+++ includes/carousel.php
@@ -11,7 +11,11 @@
         <section class="item active">
             <img src="./img/slider_1.png" alt="First slider picture">
             <section class="carousel-caption">
-                <h3>Caption Text</h3>
+
+                    <h1> Hello, traveller!! </h1>
+                    <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. </p>
+                    <p> <a class="btn btn-primary btn-large" href="#">Learn more</a></p>
+
             </section>
             <!-- Seeing if we should use few slider pictures
             </section>
