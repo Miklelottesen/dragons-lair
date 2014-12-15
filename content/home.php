2caa4f21b22b49306175cc39f6fe94a00b7e01a3
Miklelottesen <miklelottesen@gmail.com>
2014-12-15 16:56:02 +0100

    Fixed buttons (ish), added glyphicons

--- content/home.php
+++ content/home.php
@@ -1,91 +1,99 @@
-<section id="part-one">
-<section class="col-sm-12 col-md-6">
-    <h2>Events</h2>
-    <table>
-      <tr>
-        <td>
-          <img class="round_event_image" src="./img/events/card-event-aarhus.png" alt="Card event in Aarhus" />
-        </td>
-        <td>
-          <h3>Dungeons and Dragons Evening</h3>
-          <p>Weekly D&amp;D event at Dragons Lair</p>
-          <p><i class="glyphicon glyphicon-map-marker"></i>Superstreet 64, 8200 Aarhus</p>
-          <p>Monday 20 April @ 20:00</p>
-        </td>
-      </tr>
-    </table>
-    <table>
-      <tr>
-        <td>
-          <img class="round_event_image"  src="./img/events/card-event-aarhus.png" alt="Card event in Aarhus" />
-        </td>
-        <td>
-          <h3>Dungeons and Dragons Evening</h3>
-          <p>Weekly D&amp;D event at Dragons Lair</p>
-          <p><i class="glyphicon glyphicon-map-marker"></i>Superstreet 64, 8200 Aarhus</p>
-          <p>Monday 20 April @ 20:00</p>
-        </td>
-      </tr>
-    </table>
-    <table>
-      <tr>
-        <td>
-          <img class="round_event_image"  src="./img/events/card-event-aarhus.png" alt="Card event in Aarhus" />
-        </td>
-        <td>
-          <h3>Dungeons and Dragons Evening</h3>
-          <p>Weekly D&amp;D event at Dragons Lair</p>
-          <p><i class="glyphicon glyphicon-map-marker"></i>Superstreet 64, 8200 Aarhus</p>
-          <p>Monday 20 April @ 20:00</p>
-        </td>
-      </tr>
-    </table>
-    <table>
-      <tr>
-        <td>
-          <img class="round_event_image"  src="./img/events/card-event-aarhus.png" alt="Card event in Aarhus" />
-        </td>
-        <td>
-          <h3>Dungeons and Dragons Evening</h3>
-          <p>Weekly D&amp;D event at Dragons Lair</p>
-          <p><i class="glyphicon glyphicon-map-marker"></i>Superstreet 64, 8200 Aarhus</p>
-          <p>Monday 20 April @ 20:00</p>
-        </td>
-      </tr>
-    </table>
-    <button class="btn-more">More Events</button>
-</section>
-<section class="col-sm-12 col-md-6">
-    <h2>Featured</h2>
-    <img id="featured-image" class="img-responsive" alt="The Danish Magic champions" src="./img/news/den.jpg" />
-    <h3>Denmark Shocks the world of Magic</h3>
-    <small>Card Games 04/20/2014</small>
-    <p>This weekend the 2014 World Magic Cup concluded with an epic victory by underdog Denmark over Greece. The Danish Magic team shocked the world of Magic with their win..</p>
-    <table>
-      <tr>
-        <td>
-          <h4>THUMB</h4>
-        </td>
-        <td>
-          <h4>How to make fantasy armour</h4>
-          <small>August 26, <a href="#">Tutorials</a></small>
-          <small>How to make fantasy armour if you're looking for the shortest path route to making armour, then the very first thing you should do is grab a copy of this (...)</small>
-        </td>
-      </tr>
-    </table>
-    <table>
-      <tr>
-        <td>
-          <h4>THUMB</h4>
-        </td>
-        <td>
-          <h4>How to make fantasy armour</h4>
-          <small>August 26, <a href="#">Tutorials</a></small>
-          <small>How to make fantasy armour if you're looking for the shortest path route to making armour, then the very first thing you should do is grab a copy of this (...)</small>
-        </td>
-      </tr>
-    </table>
-    <button class="btn-more">More Articles</button>
-</section>
+<section class="row" id="part-one">
+  <section class="container">
+    <section class="col-sm-12 col-md-6">
+        <h2>Events</h2>
+        <table>
+          <tr>
+            <td>
+              <img class="round_event_image" src="./img/events/card-event-aarhus.png" alt="Card event in Aarhus" />
+            </td>
+            <td>
+              <h3>Dungeons and Dragons Evening</h3>
+              <p>Weekly D&amp;D event at Dragons Lair</p>
+              <p><i class="glyphicon glyphicon-map-marker"></i>Superstreet 64, 8200 Aarhus</p>
+              <p>Monday 20 April @ 20:00</p>
+            </td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>
+              <img class="round_event_image"  src="./img/events/card-event-aarhus.png" alt="Card event in Aarhus" />
+            </td>
+            <td>
+              <h3>Dungeons and Dragons Evening</h3>
+              <p>Weekly D&amp;D event at Dragons Lair</p>
+              <p><i class="glyphicon glyphicon-map-marker"></i>Superstreet 64, 8200 Aarhus</p>
+              <p>Monday 20 April @ 20:00</p>
+            </td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>
+              <img class="round_event_image"  src="./img/events/card-event-aarhus.png" alt="Card event in Aarhus" />
+            </td>
+            <td>
+              <h3>Dungeons and Dragons Evening</h3>
+              <p>Weekly D&amp;D event at Dragons Lair</p>
+              <p><i class="glyphicon glyphicon-map-marker"></i>Superstreet 64, 8200 Aarhus</p>
+              <p>Monday 20 April @ 20:00</p>
+            </td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>
+              <img class="round_event_image"  src="./img/events/card-event-aarhus.png" alt="Card event in Aarhus" />
+            </td>
+            <td>
+              <h3>Dungeons and Dragons Evening</h3>
+              <p>Weekly D&amp;D event at Dragons Lair</p>
+              <p><i class="glyphicon glyphicon-map-marker"></i>Superstreet 64, 8200 Aarhus</p>
+              <p>Monday 20 April @ 20:00</p>
+            </td>
+          </tr>
+        </table>
+    </section>
+    <section class="col-sm-12 col-md-6">
+        <h2>Featured</h2>
+        <img id="featured-image" class="img-responsive" alt="The Danish Magic champions" src="./img/news/den.jpg" />
+        <h3>Denmark Shocks the world of Magic</h3>
+        <small>Card Games 04/20/2014</small>
+        <p>This weekend the 2014 World Magic Cup concluded with an epic victory by underdog Denmark over Greece. The Danish Magic team shocked the world of Magic with their win..</p>
+        <table>
+          <tr>
+            <td>
+              <h4>THUMB</h4>
+            </td>
+            <td>
+              <h4>How to make fantasy armour</h4>
+              <small>August 26, <a href="#">Tutorials</a></small>
+              <small>How to make fantasy armour if you're looking for the shortest path route to making armour, then the very first thing you should do is grab a copy of this (...)</small>
+            </td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>
+              <h4>THUMB</h4>
+            </td>
+            <td>
+              <h4>How to make fantasy armour</h4>
+              <small>August 26, <a href="#">Tutorials</a></small>
+              <small>How to make fantasy armour if you're looking for the shortest path route to making armour, then the very first thing you should do is grab a copy of this (...)</small>
+            </td>
+          </tr>
+        </table>
+    </section>
+  </section>
+  <section class="container">
+    <section class="col-sm-12 col-md-6">
+      <button class="btn-more">More Events</button>
+    </section>
+    <section class="col-sm-12 col-md-6">
+      <button class="btn-more">More Articles</button>
+    </section>
+  </section>
 </section>
 <h2>Gallery</h2>
