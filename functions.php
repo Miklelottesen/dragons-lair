9c4b341a13af91facc86faecf20191b40dcb5f5f
Miklelottesen <miklelottesen@gmail.com>
2014-12-15 11:12:54 +0100

    Removed unneccesary files

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//Neccessary to load customized WP menu
-	function register_my_menu() {
-		register_nav_menu('header-menu',__( 'Header Menu' ));
-	}
-	add_action( 'init', 'register_my_menu' );
-	// Neccessary to enable post thumbnails:
-	add_theme_support( 'post-thumbnails' ); 
-?>
