869827e8816de945e047cfafc92e938f25ed8e2c
viliusbivainis <viliusbiv@gmail.com>
2014-12-15 06:52:48 +0100

    started news-article - and dying

---
+++ content/news-article.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row clearfix">
+        <div class="col-md-8 column">
+            <h1>Denmark Shocks the world of Magic</h1>
+            <small>Card Games 04/20/2014</small>
+            <hr>
+            Social
+            <hr>
+            <img class="img-responsive" src="./img/news/den.jpg" />
+            <h3>Denmark wins the World Magic Cup</h3>
+            <p>This weekend the 2014 World Magic Cup concluded with an epic victory by underdog Denmark over Greece. The Danish Magic team shocked the world of Magic with their win..</p>
+            <p>In this battle, team Denmark was represented by Lars Birch, Simon Nielsen, Martin Muller and Thomas Enevoldsen, which names are now engraved on the world cup trophy. </p>
+            <p></p>
+            <h4>One in a million</h4>
+            <p> Birch went on to say that the three lucky cards were a ”one in a million” and that no one had expected Denmark to win the tournament. They had hoped to finish in the top 32 out of the 72-nation competed, but they won the final by overcoming Team Greece in a nail-biting climax, thanks to some Danish luck in the final turns.
+            </p>
+        </div>
+        <div class="col-md-4 column">asd
+        </div>
+    </div>
+</div>
